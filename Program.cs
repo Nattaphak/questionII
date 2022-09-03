f66a8c3ce6080e08dff65f0366fed031a050cdac
Nattaphak Udomwasurat <nattaphak.udom@kmutt.ac.th>
2022-09-03 13:15:03 +0700

    2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double a,b,x;
+
+        double.TryParse(Console.ReadLine(), out a);
+        double.TryParse(Console.ReadLine(), out b);
+        double.TryParse(Console.ReadLine(), out x);
+
+        if(x > a || x < b)
+        {
+            Console.WriteLine("Flase");
+        }
+        else
+        {
+            Console.WriteLine("True");
+        }
+    }
+}
